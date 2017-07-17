66b9231c8fcb6e01fbd1a063cc5eb7417d7b7a1c
erickim713 <erickim713@gmail.com>
2017-07-17 17:00:04 +0900

    just need to change the status connection so that they reflect the changes.

--- static/js/ds.js
+++ static/js/ds.js
@@ -1,8 +1,9 @@
 // classes will be all defined here for effing sakes...
 // it's too complicated to write everything in one file.
-
+// TODO: i need to change the suggestion into a more healer suggestion.
 class Team {
     constructor() {
+        this.balanceState = false;
         this.members = [];
         for (var i = 0; i < 6; i++) {
             this.members.push(new Member(i)); // insert members into the Team with their id.
@@ -27,7 +28,7 @@ class Team {
             }
         }
         if(offense == 2 && tanker == 2 && healer ==2){
-            console.log("good team balance!");
+            this.balanceState = true;
         }
         else{
             offense = 2 - offense;
@@ -58,7 +59,7 @@ class Team {
         console.log("hero pick check :)");
         let suggestion = [];
         for (var i = 0; i < 6; i++) {
-            let current_enemy_counters = find_champion_weakness(enemyTeam.members[i].champion.name);
+            let current_enemy_counters = this.find_champion_weakness_counter(enemyTeam.members[i].champion.name);
             console.log(current_enemy_counters);
             console.log(enemyTeam.members[i].champion.name);
             let counter = 0;
@@ -69,24 +70,46 @@ class Team {
                 }
             }
             if(counter == 0){
-                suggestion.push(current_enemy_counters[enemyTeam.members[i].champion.name]);
+                let result = {}
+                result["champion"] = enemyTeam.members[i].champion.name;
+                result['counter'] = current_enemy_counters[enemyTeam.members[i].champion.name];
+                suggestion.push(result);
             }
         }
         return suggestion;
 
     }
-    suggestionDisplay(){
+    suggestionDisplay(theirTeam){
         //function to display the suggestions for the team to be more "successful" and have higher chance of winning.
-        console.log("suggestion display :)");
+        //suggestion is
+        this.checkBalance(); //check the balance
+        let analysisResult = this.checkPicks(theirTeam); //check the pick against the enemy team.
+        if(this.balanceState){ //if the team is balanced 2,2,2 formation then you should just see the list of champions to be selected
+            //if the team is balanced then we just have to cover the non-countered champions
+            for(var i = 0; i < analysisResult.length; i++){
+                console.log("to cover " + analysisResult[i].champion + " you will need " + analysisResult[i].counter[0] + " " + analysisResult[i].counter[1] + " " + analysisResult[i].counter[2]);
+                // console.log("hi");
+            }
+        }
+        else{
+            for(var j = 0; j <analysisResult.length; j++){
+                let subResult = this.find_champion_weakness(analysisResult[j].champion);
+                console.log("best offense to cover " + analysisResult[j].champion + " is " + subResult[0]);
+                console.log("best tanker to cover " + analysisResult[j].champion + " is " + subResult[1]);
+                console.log("best healer to cover " + analysisResult[j].champion + " is " + subResult[2]);
+            }
+        }
     }
+
     find_by_id(id){
         //access the player's information for update by finding them with their id.
         return this.members[id-1]
     }
-    find_champion_weakness(champion_name){
+
+    find_champion_weakness_counter(championName){
         let suggestPoolList = []
         for (var i = 0; i < info.length; i++) {
-            if(champion_name == info[i].Name.toLowerCase()){
+            if(championName == info[i].Name.toLowerCase()){
                 for (var j = 0; j < 3; j++) {
                     suggestPoolList.push(info[i].weakAgainst[j].name.toLowerCase());
                 }
@@ -94,8 +117,69 @@ class Team {
             }
         }
         let result = {}
-        result[champion_name] = suggestPoolList;
+        result[championName] = suggestPoolList;
         return result;
+    }
+
+    find_champion_weakness(championName){
+        //fixed finding champion counter by taking first counter respect to the enemy's pick. (for suggestion)
+        let suggestPoolList = [];
+        let offenseCounter = 0;
+        let tankerCounter = 0;
+        let healerCounter = 0;
+        for (var i = 0; i < info.length; i++) {
+            if(championName == info[i].Name.toLowerCase()){
+                for (var j = 0; j < 3; j++) {
+                    if(j == 0){
+                        console.log("looking for offense")
+                        //look for best offense counter
+                        for(var k =0; k<info.length; k++){
+                            if(new Champion(info[i].weakAgainst[k].name.toLowerCase()).role == "offense" && offenseCounter == 0){
+                                suggestPoolList.push(info[i].weakAgainst[k].name.toLowerCase());
+                                offenseCounter++;
+                                break;
+                            }
+                        }
+                    }
+                    else if(j==1){
+                        //look for the best tanker counter
+                        for(var k =0; k<info.length; k++){
+                            if(new Champion(info[i].weakAgainst[k].name.toLowerCase()).role == "tanker" && tankerCounter == 0){
+                                suggestPoolList.push(info[i].weakAgainst[k].name.toLowerCase());
+                                tankerCounter++;
+                                break;
+                            }
+                        }
+                    }
+                    else{
+                        //look for the best healer.
+                        for(var k =0; k<info.length; k++){
+                            if(new Champion(info[i].weakAgainst[k].name.toLowerCase()).role == "healer" && healerCounter == 0){
+                                suggestPoolList.push(info[i].weakAgainst[k].name.toLowerCase());
+                                offenseCounter++;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+                break;
+            }
+        }
+        let result = {}
+        result[championName] = suggestPoolList;
+        return result;
+    }
+    checkAllready(theirTeam){
+        for(var i = 0; i<6; i++){
+            if(this.members[i].champion == undefined){
+                return false;
+            }
+            if(theirTeam.members[i].champion == undefined){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
